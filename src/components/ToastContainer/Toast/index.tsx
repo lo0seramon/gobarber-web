f8c8b8b7a305d8787b06ee701f27989cc10b707b
Ramon Lima <lima.ramon13@gmail.com>
2021-07-09 22:26:24 -0300

    Implementando troca de icones do toast de acordo com o tipo da mensagem

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -1,5 +1,10 @@
 import React, { useEffect } from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
+import {
+  FiAlertCircle,
+  FiInfo,
+  FiCheckCircle,
+  FiXCircle,
+} from 'react-icons/fi';
 
 import { ToastMessage, useToast } from '../../../hooks/toast';
 
@@ -22,9 +27,15 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
     };
   }, [removeToast, message.id]);
 
+  const icons = {
+    info: <FiInfo size={24} />,
+    success: <FiCheckCircle size={24} />,
+    error: <FiAlertCircle size={24} />,
+  };
+
   return (
     <Container type={message.type} hasDescripiton={!!message.description}>
-      <FiAlertCircle size={20} />
+      {icons[message.type || 'info']}
 
       <div>
         <strong>{message.title}</strong>
